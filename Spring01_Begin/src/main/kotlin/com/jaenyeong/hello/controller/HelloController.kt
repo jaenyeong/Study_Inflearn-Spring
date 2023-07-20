87adb3a427d962fa1ec46289cf18fe856289b46e
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-20 20:30:59 +0900

    feat([01] HelloController et al): add `hello-template` html for MVC, template engine

--- Spring01_Begin/src/main/kotlin/com/jaenyeong/hello/controller/HelloController.kt
+++ Spring01_Begin/src/main/kotlin/com/jaenyeong/hello/controller/HelloController.kt
@@ -3,6 +3,8 @@ package com.jaenyeong.hello.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
@@ -11,5 +13,11 @@ class HelloController {
     fun hello(model: Model): String {
         model.addAttribute("data", "Hello!")
         return "hello"
+    }
+
+    @GetMapping("/hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String?, model: Model): String {
+        model.addAttribute("name", name)
+        return "hello-template"
     }
 }
