5cbbe2c5d568692b33e48d328a6669f84f34aada
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-21 17:42:15 +0900

    feat([01] MemberService): add `join`, `findMembers`, `findMemberById`

---
+++ Spring01_Begin/src/main/kotlin/com/jaenyeong/member/service/MemberService.kt
@@ -0,0 +1,29 @@
+package com.jaenyeong.member.service
+
+import com.jaenyeong.member.domain.Member
+import com.jaenyeong.member.repository.MemberRepository
+import com.jaenyeong.member.repository.MemoryMemberRepository
+
+class MemberService(
+    private val memberRepository: MemberRepository = MemoryMemberRepository()
+) {
+    fun join(member: Member): Long {
+        assertUniqueMemberName(member.name)
+
+        return memberRepository.save(member).id
+    }
+
+    private fun assertUniqueMemberName(givenName: String) {
+        memberRepository.findByName(givenName)?.let {
+            throw IllegalArgumentException("Member already exists: $givenName")
+        }
+    }
+
+    fun findMembers(): List<Member> {
+        return memberRepository.findAll()
+    }
+
+    fun findMemberById(id: Long): Member? {
+        return memberRepository.findById(id)
+    }
+}
