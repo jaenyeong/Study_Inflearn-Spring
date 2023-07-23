6bfb99ea9bb6b86304ebf1857b683e8e0a5143ae
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-23 12:28:17 +0900

    chore([01] SpringConfig): add Spring Configuration

---
+++ Spring01_Begin/src/main/kotlin/com/jaenyeong/SpringConfig.kt
@@ -0,0 +1,20 @@
+package com.jaenyeong
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SpringConfig {
+    // 아래와 같이 빈을 등록할 수도 있음
+    // 이미 등록(스캔)된 빈을 설정하면 에러가 발생함
+    //// The bean 'memberService', defined in class path resource [com/jaenyeong/SpringConfig.class],
+    //// could not be registered. A bean with that name has already been defined in file
+//    @Bean
+//    fun memberService(): MemberService {
+//        return MemberService(memberRepository())
+//    }
+//
+//    @Bean
+//    fun memberRepository(): MemberRepository {
+//        return MemoryMemberRepository()
+//    }
+}
