1a3de54a87fa19354c054d4a7f5abd060139924e
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-23 17:36:33 +0900

    chore([01] SpringConfig): change repository bean to `JdbcMemberRepository` from `MemoryMemberRepository`

--- Spring01_Begin/src/main/kotlin/com/jaenyeong/SpringConfig.kt
+++ Spring01_Begin/src/main/kotlin/com/jaenyeong/SpringConfig.kt
@@ -1,13 +1,16 @@
 package com.jaenyeong
 
+import com.jaenyeong.member.repository.JdbcMemberRepository
 import com.jaenyeong.member.repository.MemberRepository
-import com.jaenyeong.member.repository.MemoryMemberRepository
 import com.jaenyeong.member.service.MemberService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
 
 @Configuration
-class SpringConfig {
+class SpringConfig(
+    private val dataSource: DataSource
+) {
     // 아래와 같이 빈을 등록할 수도 있음
     // 이미 등록(스캔)된 빈을 설정하면 에러가 발생함
     //// The bean 'memberService', defined in class path resource [com/jaenyeong/SpringConfig.class],
@@ -19,6 +22,7 @@ class SpringConfig {
 
     @Bean
     fun memberRepository(): MemberRepository {
-        return MemoryMemberRepository()
+//        return MemoryMemberRepository()
+        return JdbcMemberRepository(dataSource)
     }
 }
