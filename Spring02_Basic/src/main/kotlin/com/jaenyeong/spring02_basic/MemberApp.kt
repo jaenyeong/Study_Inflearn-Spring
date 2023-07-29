a2058ee52a5e4f914cc1f83f058905f7b84d736a
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-29 22:31:07 +0900

    refactor([02] MemberApp et al): change `main function` code to `comment` to avoid `gradle build` failure

--- Spring02_Basic/src/main/kotlin/com/jaenyeong/spring02_basic/MemberApp.kt
+++ Spring02_Basic/src/main/kotlin/com/jaenyeong/spring02_basic/MemberApp.kt
@@ -1,22 +1,18 @@
 package com.jaenyeong.spring02_basic
 
-import com.jaenyeong.spring02_basic.member.Grade.VIP
-import com.jaenyeong.spring02_basic.member.Member
-import com.jaenyeong.spring02_basic.member.MemberService
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
-
+// main 함수가 많아 Build 실패하여 주석처리
 class MemberApp
 
-fun main(args: Array<String>) {
-    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
-
-    val memberService = context.getBean("memberService", MemberService::class.java)
-
-    val newMember = Member(id = 1L, name = "member1", grade = VIP)
-    memberService.join(newMember)
-
-    val foundMember = memberService.findMember(newMember.id)
-
-    println("newMember = $newMember")
-    println("foundMember = $foundMember")
-}
+//fun main(args: Array<String>) {
+//    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
+//
+//    val memberService = context.getBean("memberService", MemberService::class.java)
+//
+//    val newMember = Member(id = 1L, name = "member1", grade = VIP)
+//    memberService.join(newMember)
+//
+//    val foundMember = memberService.findMember(newMember.id)
+//
+//    println("newMember = $newMember")
+//    println("foundMember = $foundMember")
+//}
--- Spring02_Basic/src/main/kotlin/com/jaenyeong/spring02_basic/OrderApp.kt
+++ Spring02_Basic/src/main/kotlin/com/jaenyeong/spring02_basic/OrderApp.kt
@@ -1,23 +1,17 @@
 package com.jaenyeong.spring02_basic
 
-import com.jaenyeong.spring02_basic.member.Grade.VIP
-import com.jaenyeong.spring02_basic.member.Member
-import com.jaenyeong.spring02_basic.member.MemberService
-import com.jaenyeong.spring02_basic.order.OrderService
-import com.jaenyeong.spring02_basic.product.Product
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
-
+// main 함수가 많아 Build 실패하여 주석처리
 class OrderApp
 
-fun main(args: Array<String>) {
-    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
-
-    val memberService = context.getBean("memberService", MemberService::class.java)
-    val orderService = context.getBean("orderService", OrderService::class.java)
-    val newMember = Member(id = 1L, name = "member1", grade = VIP)
-    memberService.join(newMember)
-
-    val createdOrder = orderService.createOrder(newMember.id, product = Product(name = "Product1", price = 30_000))
-
-    println("Created order = $createdOrder")
-}
+//fun main(args: Array<String>) {
+//    val context = AnnotationConfigApplicationContext(AppConfig::class.java)
+//
+//    val memberService = context.getBean("memberService", MemberService::class.java)
+//    val orderService = context.getBean("orderService", OrderService::class.java)
+//    val newMember = Member(id = 1L, name = "member1", grade = VIP)
+//    memberService.join(newMember)
+//
+//    val createdOrder = orderService.createOrder(newMember.id, product = Product(name = "Product1", price = 30_000))
+//
+//    println("Created order = $createdOrder")
+//}
